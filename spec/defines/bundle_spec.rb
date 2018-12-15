017ce0d711ecd9547de64e41079af859f9c1efcb
Bryan Wyatt <brwyatt@gmail.com>
2018-12-15 11:59:12 -0800

    Add basic testing for vim::bundle

--- spec/defines/bundle_spec.rb
+++ spec/defines/bundle_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 describe 'vim::bundle' do
-  let(:title) { 'namevar' }
+  let(:title) { 'test-bundle' }
   let(:params) do
-    {}
+    {
+      'owner'   => 'root',
+      'repo'    => 'https://github.com/tpope/vim-sensible.git',
+      'vim_dir' => '/tmp',
+    }
   end
 
   on_supported_os.each do |os, os_facts|
@@ -11,6 +15,15 @@ describe 'vim::bundle' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+      it {
+        is_expected.to contain_vcsrepo("vim bundle: /tmp/bundle/test-bundle").with(
+          'ensure'   => 'latest',
+          'provider' => 'git',
+        )
+      }
+      it { is_expected.to contain_class('git') }
     end
   end
 end
+
+# vim: ts=2 sts=2 sw=2 expandtab
