9ae60d928fe46fd0170f1783f95e810abff871ef
Bryan Wyatt <brwyatt@gmail.com>
2018-12-15 12:33:58 -0800

    Make sure directory exists so it isn't stompped
    This is just to avoid cases where someone may wish to purge their main
    config dir. This makes sure these dirs remain.

--- manifests/bundle.pp
+++ manifests/bundle.pp
@@ -10,15 +10,26 @@ define vim::bundle(
   String $vim_dir,
   String $bundle_name = $name,
   Enum['present', 'absent'] $ensure = 'present',
+  String $group = $owner,
   String $provider = 'git',
   String $revision = 'master',
 ) {
   $path = "${vim_dir}/bundle/${bundle_name}"
+
+  file { $path:
+    ensure => directory,
+    owner  => $owner,
+    group  => $group,
+    mode   => '0775',
+    before => Vcsrepo["vim bundle: ${path}"],
+  }
+
   vcsrepo { "vim bundle: ${path}":
     ensure   => latest,
     path     => $path,
     provider => $provider,
     owner    => $owner,
+    group    => $group,
     source   => $repo,
     revision => $revision,
   }
